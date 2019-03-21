e2bb8fa1799b4e75169576c288b5b73008589918
stephen <stephen@plugable.com>
2019-03-21 12:45:45 -0700

    updated feature page reorganized templates removed uncessary plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,7 +18,7 @@ DEFAULT_LANG = u'en'
 
 DIRECT_TEMPLATES = ['categories', 'index', 'archives', 'authors', 'tags', 'search', 'sitemap']
 
-GOOGLE_ANALYTICS = 'UA-73097851-1'
+GOOGLE_ANALYTICS = ''
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -43,16 +43,11 @@ MENUITEMS_RIGHT = (
 # Removed:  ('About', '/about/')
 
 # Post Settings
-DEFAULT_PAGINATION = 8
+DEFAULT_PAGINATION = 5
 DEFAULT_DATE = 'fs'
 
 # Share Buttons
 ADDTHIS_PROFILE = 'ra-53cd839229a71296'
-# SHARIFF = True
-# SHARIFF_LANG = 'en'
-# SHARIFF_THEME = 'grey'
-# TWITTER_USERNAME = ''
-# SHARIFF_TWITTER_VIA = True
 
 # Paths
 PATH = 'content'
@@ -62,11 +57,14 @@ ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{slug}.html'
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{slug}.html'
 INDEX_SAVE_AS = 'blog/index.html'
 TEMPLATE_PAGES = {
-    'templates/google96a16df7dbeb77e9.html': 'google96a16df7dbeb77e9.html',
+    'templates/validation/google96a16df7dbeb77e9.html': 'google96a16df7dbeb77e9.html',
+    'templates/validation/atlassian-domain-verification.html': 'atlassian-domain-verification.html',
     'templates/flywheels.html': 'flywheels.html',
-    'templates/index.html': 'index.html',
-    'templates/atlassian-domain-verification.html': 'atlassian-domain-verification.html',
-    'templates/pricing.html': 'pricing/index.html'
+    'templates/pages/index.html': 'index.html',
+    'templates/pages/pricing.html': 'pricing/index.html',
+    'templates/pages/features.html': 'features/index.html',
+    'templates/pages/support.html': 'support/index.html',
+    'templates/pages/apphelp.html': 'apphelp/index.html',
 }
 SITEMAP_SAVE_AS = 'sitemap.xml'
 
@@ -83,7 +81,6 @@ DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
 DISPLAY_CATEGORIES_ON_SIDEBAR = True
-BOOTSTRAP_NAVBAR_INVERSE = True
 SITELOGO = 'images/logo_name.png'
 SITELOGO_SIZE = '140px'
 HIDE_SITENAME = True
@@ -92,7 +89,7 @@ BACK_TO_TOP = False
 
 # Plugins
 PLUGIN_PATHS = ["plugins"]
-PLUGINS = ["thumbnailer", "tipue_search", "i18n_subsites"]
+PLUGINS = ["thumbnailer", "tipue_search",]
 
 # Thumbnailer
 IMAGE_PATH = 'images'
