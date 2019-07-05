cb0da1c73be936c76cc57d4d0fa568c0bbcc84ca
ritu <ritu@efficientera.com>
2019-07-05 13:05:54 -0700

    Testing changes

--- fabfile.py
+++ fabfile.py
@@ -69,13 +69,13 @@ def publish():
     local('pelican -s publishconf.py')
 
 
-def s3_upload():
-    publish()
-    local('python C:\Python27\Scripts\s3cmd sync output/ --acl-public --delete-removed --guess-mime-type s3://%s/' % S3BUCKET)
+# def s3_upload():
+#     publish()
+#     local('python C:\Python27\Scripts\s3cmd sync output/ --acl-public --delete-removed --guess-mime-type s3://%s/' % S3BUCKET)
 
 # Changed to accomodate Mac path:
-# def mac_upload():
-#        publish()
-#        local('python /usr/local/bin/s3cmd sync output/ --acl-public --delete-removed --guess-mime-type s3://efficientera.com/')
+def mac_upload():
+       publish()
+       local('python /usr/local/bin/s3cmd sync output/ --acl-public --delete-removed --guess-mime-type s3://efficientera.com/')
 
        # local('python /usr/local/Cellar/s3cmd/1.6.1/bin/s3cmd sync output/ --acl-public --delete-removed --guess-mime-type s3://efficientera.com/')
